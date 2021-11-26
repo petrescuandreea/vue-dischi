5ee6dca3c6b2cb687257fa855fdff77bf6f48de7
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-26 13:05:53 +0100

    utilizzato componente Albumlist

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <!-- componente Albumlist  -->
+    <Albumlist/>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Albumlist from './components/Albumlist.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Albumlist
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

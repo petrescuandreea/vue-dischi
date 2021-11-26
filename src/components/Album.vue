9be590f7459f5e8afd4ae7b8e1e95ad5b4dad948
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-26 13:05:05 +0100

    creato componente figlio

---
+++ src/components/Album.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="album">
+        <img :src="details.poster" :alt="details.author">
+        <h1>{{ details.title }}</h1>
+        <span>{{ details.author }}</span>
+        <span>{{ details.year }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Album',
+  props: {
+      details: Object
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .album {
+        text-align: center;
+        width: calc(100% / 5 - 20px);
+        min-height: 400px;
+        padding: 10px;
+        background-color: rgba(46,58,70,255);
+        margin: 10px;
+
+
+        h1 {
+            color: white;
+            font-size: 25px;
+            text-transform: uppercase;
+            margin: 15px;
+        }
+
+        img {
+            max-width: 100%;
+        }
+
+        span {
+            display: block;
+            color: rgba(127,126,118,255);
+        }
+    }
+    
+</style>

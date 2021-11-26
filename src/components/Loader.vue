588bf6a7514ff15abbf923faa66ebd424ccda98d
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-26 17:14:29 +0100

    creato componente

---
+++ src/components/Loader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="loader">
+      Sto caricando ...
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Loader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    #loader {
+            color: white;
+            font-size: 35px;
+            height: 100vh;
+            padding: 20px 0 0 40px;
+        }
+</style>

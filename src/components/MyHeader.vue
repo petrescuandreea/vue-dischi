480332fdc728491e60018b7a561f0b9ddbd957c4
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-26 16:52:20 +0100

    creato componente

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,26 @@
+<template>
+   <div id="header">
+     <i class="fab fa-spotify"></i>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  #header {
+    background-color: rgba(46,58,70,255);
+    padding: 10px 20px;
+    
+    i {
+      color: rgba(30,215,96,255);
+      font-size: 50px;
+    }
+  }
+    
+</style>

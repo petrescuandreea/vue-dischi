3b172dd16ec71c4c7157e44c733b6d214d9b3499
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-29 15:43:15 +0100

    creato componente

---
+++ src/components/FilterAlbum.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- associo l'evento change  -->
+    <label> Seleziona genere
+        <select @change="$emit('filter', $event)">
+            <option value="all">All</option>
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+            <option value="jazz">Jazz</option>
+            <option value="metal">Metal</option>
+        </select>
+    </label>
+</template>
+
+<script>
+export default {
+  name: 'FilterAlbum',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+    
+</style>
